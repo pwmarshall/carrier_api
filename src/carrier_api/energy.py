bb4cb16a007f541f7280606f9e2e31c1cb941fea
Peter M <pwmarshall@gmail.com>
2024-12-24 14:37:26 -0600

    Formating and monthly logging

--- src/carrier_api/energy.py
+++ src/carrier_api/energy.py
@@ -4,6 +4,45 @@ from .util import safely_get_json_value
 
 _LOGGER = logging.getLogger(__name__)
 
+class Energy_Usage:
+    def __init__(self, raw_usage_json):
+        self.times = []
+        periods = safely_get_json_value(raw_usage_json, "period")
+        for i in range(6):
+            self.times.append(periods[i])
+
+    def __repr__(self):
+        return {
+            "day1": self.times[0],
+            "day2": self.times[1],
+            "month1": self.times[2],
+            "month2": self.times[3],
+            "year1": self.times[4],
+            "year2": self.times[5],
+        }
+
+    def __str__(self):
+        return str(self.__repr__())
+
+class Energy_Cost:
+    def __init__(self, raw_cost_json):
+        self.times = []
+        periods = safely_get_json_value(raw_cost_json, "period")
+        for i in range(6):
+            self.times.append(periods[i])
+
+    def __repr__(self):
+        return {
+            "day1": self.times[0],
+            "day2": self.times[1],
+            "month1": self.times[2],
+            "month2": self.times[3],
+            "year1": self.times[4],
+            "year2": self.times[5],
+        }
+
+    def __str__(self):
+        return str(self.__repr__())
 
 class Energy:
     usage: str = None
@@ -17,17 +56,17 @@ class Energy:
         self.refresh()
 
     def refresh(self):
-        self.raw_profile_json = self.system.api_connection.get_energy(
+        self.raw_energy_json = self.system.api_connection.get_energy(
             system_serial=self.system.serial
         )
-        _LOGGER.debug(f"raw_profile_json:{self.raw_profile_json}")
-        self.usage = safely_get_json_value(self.raw_profile_json, "usage")
-        self.cost = safely_get_json_value(self.raw_profile_json, "cost")
+        _LOGGER.debug(f"raw_profile_json:{self.raw_energy_json}")
+        self.usage = Energy_Usage(safely_get_json_value(self.raw_energy_json, "usage"))
+        self.cost = Energy_Cost(safely_get_json_value(self.raw_energy_json, "cost"))
 
     def __repr__(self):
         return {
-            "usage": self.usage,
-            "cost": self.cost,
+            "usage": self.usage.__repr__(),
+            "cost": self.cost.__repr__(),
         }
 
     def __str__(self):
--- src/carrier_api/query.py
+++ src/carrier_api/query.py
@@ -1,5 +1,6 @@
 from .api_connection import ApiConnection
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 import csv
 
 from .passwords import USERNAME, PASSWORD
@@ -7,8 +8,15 @@ from .passwords import USERNAME, PASSWORD
 import logging
 logging.basicConfig(level=logging.INFO) #DEBUG, INFO, WARNING, ERROR, CRITICAL
 
+from enum import Enum
 
 _LOGGER = logging.getLogger(__name__)
+
+class TimePeriod(Enum):
+    DAY = 1
+    MONTH = 2
+    YEAR = 3
+
 
 def authenticate():
     # Authenticate with the API    
@@ -19,34 +27,66 @@ def authenticate():
     return api
     
 
+def log_time(type:TimePeriod, keys, systemDict):
+    if type == TimePeriod.DAY:
+        date = datetime.today().date()
+    elif type == TimePeriod.MONTH:
+        date = datetime.today().date().replace(day=1)
+    elif type == TimePeriod.YEAR:
+        date = datetime.today().date().replace(month=1, day=1)
+
+    fileName = f"{systemDict['name']}_system_data_{type.name}.csv"
+    try:
+        with open(fileName, mode='r') as file:
+            reader = csv.DictReader(file)
+            allDates = [row["Date"] for row in reader]
+    except FileNotFoundError:
+        allDates = []
+
+
+    with open(fileName, mode='a', newline="\n") as file:
+        writer = csv.writer(file)
+        if file.tell() == 0:
+            writer.writerow(keys)
+
+
+        for i in [0,1]:
+            if type == TimePeriod.DAY:
+                newDate = str(date - timedelta(days=i+1))
+            elif type == TimePeriod.MONTH:
+                newDate = str(date - relativedelta(months=i+1))
+            elif type == TimePeriod.YEAR:
+                newDate = str(date - relativedelta(years=i+1))
+            usage = [newDate]
+
+            for j in keys:
+                if j != "Date":
+                    usage.append(systemDict["energy"]["usage"][f"{type.name.lower()}{i+1}"][j])
+
+            # _LOGGER.info(usage)
+
+            _LOGGER.info(f"At {systemDict['name']} gas usage is {usage[keys.index('gas')]} for {newDate}")
+            if newDate not in allDates:
+                writer.writerow(usage)
+
+
+
 if __name__ == "__main__":
     api: ApiConnection = authenticate()
     
     systems = api.get_systems()
-    date = datetime.today().date()
 
     for system in systems:
         systemDict = system.__repr__()
+        keys = [i if i != "$" else "Date" for i in systemDict["energy"]["usage"][f"day1"].keys()]
+        _LOGGER.info(f"Keys are {keys}")
 
-        try:
-            with open(f"{systemDict['name']}_system_data.csv", mode='r') as file:
-                reader = csv.DictReader(file)
-                allDates = [row["Date"] for row in reader]
-        except FileNotFoundError:
-            allDates = []
-
-
-        with open(f"{systemDict['name']}_system_data.csv", mode='a', newline="\n") as file:
-            writer = csv.writer(file)
-            if file.tell() == 0:
-                writer.writerow(["Date", "Usage"])
+        log_time(TimePeriod.DAY, keys, systemDict)
+        log_time(TimePeriod.MONTH, keys, systemDict)
+        # log_time(TimePeriod.YEAR, keys, systemDict) #current year is logged through todays date which makes logging weird
 
 
-            for i in [0,1]:
-                usage = int(systemDict["energy"]["usage"]["period"][i]["gas"])
-                newDate = str(date - timedelta(days=i+1))
-                _LOGGER.info(f"At {systemDict['name']} usage is {usage} for {newDate}")
-                if newDate not in allDates:
-                    writer.writerow([newDate, usage])
+
+        
 
 
