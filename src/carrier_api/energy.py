e0a082421a8f2d0e032f01dfdc8d9c43e07e4509
Peter M <pwmarshall@gmail.com>
2024-12-28 16:52:43 -0600

    add logging functions to energy class

--- src/carrier_api/energy.py
+++ src/carrier_api/energy.py
@@ -1,6 +1,11 @@
+import csv
 import logging
 
+from .const import TimePeriod
 from .util import safely_get_json_value
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -71,3 +76,56 @@ class Energy:
 
     def __str__(self):
         return str(self.__repr__())
+    
+    def get_usage_types(self) -> list:
+        return [i for i in self.usage.__repr__()["day1"].keys() if i != "$"]
+    
+    def log_usage(self, type:TimePeriod, keys, fileName):
+        if type == TimePeriod.DAY:
+            date = datetime.today().date()
+        elif type == TimePeriod.MONTH:
+            date = datetime.today().date().replace(day=1)
+        elif type == TimePeriod.YEAR:
+            _LOGGER.info("Year Logging Not Fully Implemented Yet")
+            date = datetime.today().date().replace(month=1, day=1)
+        else:
+            raise ValueError(f"{type} is not a valid Logging Time Period")
+        
+        if keys[0] != "Date":
+            keys.insert(0,"Date")
+
+        if fileName[-4:] != ".csv":
+            raise ValueError(f"{fileName} is not a csv file")
+
+        try:
+            with open(fileName, mode='r') as file:
+                reader = csv.DictReader(file)
+                allDates = [row["Date"] for row in reader]
+        except FileNotFoundError:
+            allDates = []
+
+
+        with open(fileName, mode='a', newline="\n") as file:
+            writer = csv.writer(file)
+            if file.tell() == 0:
+                writer.writerow(keys)
+
+            for i in [0,1]: #api only returns 2 days/months
+                if type == TimePeriod.DAY:
+                    newDate = str(date - timedelta(days=i+1))
+                elif type == TimePeriod.MONTH:
+                    newDate = str(date - relativedelta(months=i+1))
+                elif type == TimePeriod.YEAR:
+                    newDate = str(date - relativedelta(years=i+1))
+
+                usage = [newDate]
+
+                for j in keys:
+                    if j != "Date":
+                        try:
+                            usage.append(self.__repr__()["usage"][f"{type.name.lower()}{i+1}"][j])
+                        except KeyError:
+                            raise KeyError(f"{j} is not a valid usage type")
+
+                if newDate not in allDates:
+                    writer.writerow(usage)
--- src/carrier_api/query.py
+++ src/carrier_api/query.py
@@ -4,86 +4,28 @@ from dateutil.relativedelta import relativedelta
 import csv
 
 from .passwords import USERNAME, PASSWORD
+from .const import TimePeriod
 
 import logging
 logging.basicConfig(level=logging.INFO) #DEBUG, INFO, WARNING, ERROR, CRITICAL
 
-from enum import Enum
-
 _LOGGER = logging.getLogger(__name__)
 
-class TimePeriod(Enum):
-    DAY = 1
-    MONTH = 2
-    YEAR = 3
 
-
-def authenticate():
-    # Authenticate with the API    
+if __name__ == "__main__":
     api = ApiConnection(USERNAME, PASSWORD)
     if api == None:
         raise Exception("Authentication failed")
     
-    return api
-    
-
-def log_time(type:TimePeriod, keys, systemDict):
-    if type == TimePeriod.DAY:
-        date = datetime.today().date()
-    elif type == TimePeriod.MONTH:
-        date = datetime.today().date().replace(day=1)
-    elif type == TimePeriod.YEAR:
-        date = datetime.today().date().replace(month=1, day=1)
-
-    fileName = f"{systemDict['name']}_system_data_{type.name}.csv"
-    try:
-        with open(fileName, mode='r') as file:
-            reader = csv.DictReader(file)
-            allDates = [row["Date"] for row in reader]
-    except FileNotFoundError:
-        allDates = []
-
-
-    with open(fileName, mode='a', newline="\n") as file:
-        writer = csv.writer(file)
-        if file.tell() == 0:
-            writer.writerow(keys)
-
-
-        for i in [0,1]:
-            if type == TimePeriod.DAY:
-                newDate = str(date - timedelta(days=i+1))
-            elif type == TimePeriod.MONTH:
-                newDate = str(date - relativedelta(months=i+1))
-            elif type == TimePeriod.YEAR:
-                newDate = str(date - relativedelta(years=i+1))
-            usage = [newDate]
-
-            for j in keys:
-                if j != "Date":
-                    usage.append(systemDict["energy"]["usage"][f"{type.name.lower()}{i+1}"][j])
-
-            # _LOGGER.info(usage)
-
-            _LOGGER.info(f"At {systemDict['name']} gas usage is {usage[keys.index('gas')]} for {newDate}")
-            if newDate not in allDates:
-                writer.writerow(usage)
-
-
-
-if __name__ == "__main__":
-    api: ApiConnection = authenticate()
-    
     systems = api.get_systems()
 
     for system in systems:
-        systemDict = system.__repr__()
-        keys = [i if i != "$" else "Date" for i in systemDict["energy"]["usage"][f"day1"].keys()]
+        keys = system.energy.get_usage_types()
+
         _LOGGER.info(f"Keys are {keys}")
 
-        log_time(TimePeriod.DAY, keys, systemDict)
-        log_time(TimePeriod.MONTH, keys, systemDict)
-        # log_time(TimePeriod.YEAR, keys, systemDict) #current year is logged through todays date which makes logging weird
+        system.energy.log_usage(TimePeriod.DAY, keys, f"{system.__repr__()['name']}_system_data_day.csv")
+        system.energy.log_usage(TimePeriod.MONTH, keys, f"{system.__repr__()['name']}_system_data_month.csv")
 
 
 
